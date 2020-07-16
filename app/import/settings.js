ad6a72263a88de9fdbfbcffb39327cf3227f745b
Lukas Herzog <git@lukasherzog.de>
2020-07-16 17:15:27 +0200

    add settings module

---
+++ app/import/settings.js
@@ -0,0 +1,69 @@
+const Store = require('electron-store');
+
+const schema = {
+    cgsServer: {
+        IP: {
+            type: 'string',
+            format: 'ipv4',
+            default: '127.0.0.1',
+        },
+        Port: {
+            type: 'integer',
+            min: 1000,
+            default: 5250,
+        },
+        Queue: {
+            type: 'integer',
+            min: 1,
+            max: 2,
+            default: 1,
+        },
+    },
+    cgtTemplate: {
+        SlotA: {
+            Name: {
+                type: 'string',
+                default: '',
+            },
+            Key1: {
+                type: 'string',
+                default: 'f0',
+            },
+            Key2: {
+                type: 'string',
+                default: 'f1',
+            },
+            Layer: {
+                type: 'integer',
+                min: 1,
+                default: 20,
+            },
+        },
+        SlotB: {
+            Name: {
+                type: 'string',
+                default: '',
+            },
+            Key1: {
+                type: 'string',
+                default: 'f0',
+            },
+            Key2: {
+                type: 'string',
+                default: 'f1',
+            },
+            Layer: {
+                type: 'integer',
+                min: 1,
+                default: 20,
+            },
+        },
+    },
+};
+
+module.exports = new Store({
+    name: 'settings',
+    fileExtension: 'config',
+    watch: true,
+    schema: schema,
+});
