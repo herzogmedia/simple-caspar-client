6c82e910188f7d6263778a65a26e9ede2a125693
Lukas Herzog <git@lukasherzog.de>
2020-07-20 21:32:23 +0200

    populate Library and add to lib from slot a

--- app/public/js/mainWindow.js
+++ app/public/js/mainWindow.js
@@ -14,6 +14,7 @@ const autoA = document.querySelector('#auto-a');
 const line1A = document.querySelector('#a-line1');
 const line2A = document.querySelector('#a-line2');
 const durationA = document.querySelector('#a-ap-interval');
+const addLibA = document.querySelector('#add-lib-a');
 
 // Slot B
 const playB = document.querySelector('#play-b');
@@ -33,6 +34,7 @@ ipc.on('cgsConnection', (event, connection) => {
 playA.addEventListener('click', playSlotA);
 stopA.addEventListener('click', stopSlotA);
 autoA.addEventListener('click', autoSlotA);
+addLibA.addEventListener('click', addLibSlotA);
 
 // Slot A Buttons
 playB.addEventListener('click', playSlotB);
@@ -68,6 +70,16 @@ function autoSlotA(e) {
         duration: parseInt(durationA.value)
     };
     ipc.send('cg-auto', playData);
+}
+
+function addLibSlotA(e) {
+    e.preventDefault();
+    const data = {
+        line1: line1A.value,
+        line2: line2A.value
+    };
+    ipc.send('lib-add', data);
+    populateLibrary();
 }
 
 //-----Playout Function SLOT B -------//
@@ -116,3 +128,33 @@ function populateCGStatus(connection) {
         ServerStatusIcon.classList.remove('green-text');
     }
 }
+
+// ------- Library ----------//
+
+const libraryTable = document.querySelector('#library-hits');
+const secondaryContent = `<div class="secondary-content">
+<button class="btn-small waves-effect blue-grey"> <i class="material-icons">arrow_upward</i>Load A</button>
+<button class="btn-small waves-effect blue-grey"><i class="material-icons">arrow_upward</i>Load B</button>
+<button class="btn-floating indigo darken-3"><i class="material-icons">edit</i></button>
+<button class="btn-floating red darken-3"><i class="material-icons">clear</i></button>
+</div>`;
+
+// Populate Library
+function populateLibrary() {
+    const lowerThirds = ipc.sendSync('lib-getLatest', 3);
+    let html;
+    if (lowerThirds.length < 1) {
+        html = '<h6><em>The Library is currently empty.</em></h6></br>';
+    } else {
+        html = '<ul class="collection grey darken-3" style="border: none;">';
+        lowerThirds.forEach((lt) => {
+            html += `<li class="collection-item grey darken-2" id="${lt._id}">
+            <strong>${lt.line1}</strong><br />${lt.line2} 
+            ${secondaryContent}</li>`;
+        });
+        html += '</ul>';
+    }
+    libraryTable.innerHTML = html;
+}
+
+populateLibrary();
