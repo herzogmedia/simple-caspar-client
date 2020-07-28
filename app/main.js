65b2ddb5a0d55989a43ead10e13573e617031525
Lukas Herzog <git@lukasherzog.de>
2020-07-29 00:51:20 +0200

    add dev-env-var

--- app/main.js
+++ app/main.js
@@ -17,10 +17,7 @@ const {
 const { app, BrowserWindow, Menu, dialog } = electron;
 const ipc = electron.ipcMain;
 
-// Set env
-process.env.NODE_ENV = 'development';
-
-const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isDev = process.env.NODE_ENV == 'development' ? true : false;
 const isMac = process.platform === 'darwin' ? true : false;
 
 let mainWindow;
